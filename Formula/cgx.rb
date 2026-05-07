c0f220e06c7624809b527c212a5d0d8fbe108d55
AayushBahukhandi <aayushpotter555@gmail.com>
2026-05-07 14:58:34 +0530

    cgx 0.1.2

--- Formula/cgx.rb
+++ Formula/cgx.rb
@@ -1,17 +1,17 @@
 class Cgx < Formula
   desc "Turn any Git repository into a queryable knowledge graph"
   homepage "https://github.com/AayushBahukhandi/cgx"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/AayushBahukhandi/cgx/releases/download/v#{version}/cgx-v#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "REPLACE_WITH_ARM64_SHA256"
+    sha256 "11fdeeff3bbb29a08976b78b3436df7f3ec81bb0abcc1139a989f3e1cf2b9406"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/AayushBahukhandi/cgx/releases/download/v#{version}/cgx-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "REPLACE_WITH_INTEL_SHA256"
+    sha256 "d486a3b14dc13bee765dc3286192e99f6360e723f67d7ae08931ff15e6f0a94a"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/AayushBahukhandi/cgx/releases/download/v#{version}/cgx-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "REPLACE_WITH_LINUX_SHA256"
+    sha256 "35e3abab43102e2b5343cdf45b08c2d5cbacca699b15c07230df8e9b41194163"
   end
   def install
     bin.install "cgx"
